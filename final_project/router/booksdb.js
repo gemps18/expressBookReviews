8a0334783ebdaa8e201f389e9720551892bf784c
gemps18 <150620155+gemps18@users.noreply.github.com>
2023-12-28 23:38:11 +0100

    Update booksdb.js
    update 2.0

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"id": "1","author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
+      2: {"id": "2","author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+      3: {"id": "3","author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+      4: {"id": "4","author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
+      5: {"id": "5","author": "Unknown","title": "The Book Of Job", "reviews": {} },
+      6: {"id": "6","author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+      7: {"id": "7","author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+      8: {"id": "8","author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+      9: {"id": "9","author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+      10: {"id": "10","author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
 }
 
 module.exports=books;
