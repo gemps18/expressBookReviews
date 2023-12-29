f96d55c821d5769996dc4d7c7de6b63ef170747a
gemps18 <150620155+gemps18@users.noreply.github.com>
2023-12-29 23:51:15 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,18 +2,22 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+const axios = require("axios");
 const public_users = express.Router();
 
+public_users.use(express.json());
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  const username = req.body.username;
-  const password = req.body.password;
+  let username = req.body.username;
+  let password = req.body.password;
+  console.log(req.body)
   if(username && password){
     if(!isValid(username)){
       users.push({"username": username, "password": password});
       return res.status(200).json({message: "User Successfully registred. You can now login."})
-    }else{
+    }
+    else{
       return res.status(404).json({message: "User already exist!"})
     }
   }else{
