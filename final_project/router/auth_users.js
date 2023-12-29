b5367214e85ac8cb9bf7681fe6b1ffcf1b01d630
gemps18 <150620155+gemps18@users.noreply.github.com>
2023-12-29 20:42:32 +0100

    Update auth_users.js
    update 3.2

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -35,67 +35,81 @@ if(validusers.length >0){
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  const username = req.body.username
-  const password = req.body.password
-  if(!username || !password){
-      return res.status(400).json({message: "Error logging in!"});
-  }
-  if(authenticatedUser(username, password)){
-    let accessToken = jwt.sign({data: password}, 'access', {expiresIn: 60*60});
-    req.session.authorization = {
-        accessToken, username
+    //Write your code here
+    const username = req.body.username
+    const password = req.body.password
+    if(!username || !password){
+         return res.status(400).json({message: "Error logging in!"});
     }
-    return res.status(200).send("User successfully logged in");
-  }else{
-    return res.status(208).json({message: "Invalid Login. Check username and password"});
-  }
+    if(authenticatedUser(username, password)){
+       let accessToken = jwt.sign({data: password}, 'access', {expiresIn: 60*60});
+       req.session.authorization = {
+           accessToken, username
+       }
+       return res.status(200).send("User successfully logged in");
+    }else{
+       return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }
+   });
+   
+   // Add a book review
+   router.post("/auth/review", async (req, res) => {
+    const review = {
+        username: req.session.authorization.username,
+        isbn: req.body.isbn,
+        review: req.body.review
+    };
+
+    const bookReview = await BookReview.findOne({isbn: review.isbn, username: review.username});
+
+    if (bookReview) {
+        await BookReview.updateOne({isbn: review.isbn, username: review.username}, {$set: {review: review.review}});
+    } else {
+        await BookReview.create(review);
+    }
+
+    res.status(200).json({message: "Review has been added successfully!"});
 });
 
-function findBookById(id) {
-    return books.find(book => book.id === id);
-}
-
-// Add a book review
-regd_users.put("/auth/review/:id", (req, res) => {
-    let id = req.params.id;
-    let review = req.query.review;
-    let username = req.session.authorization.username;
-    let book = findBookById(id);
-
-    if (book) {
-        let obj = book.reviews;
-        let tipo = typeof(obj);
-        if(obj[username]){
-            obj[username] = review;
-            return res.status(200).json({message: "Review same user saved ***"+ tipo +"**** " + JSON.stringify(book)});
-        }else{
-            obj[username] = review;
-            return res.status(200).json({message: "Different user Review saved " + JSON.stringify(book)});
-        }
-    } else {
-        return res.status(404).json({message: "Book not found!"});
-    }
-});
-
-regd_users.delete("/auth/review/:id", (req, res)=>{
-    let id = req.params.id;
-    let username = req.session.authorization.username;
-    let book = findBookById(id);
-
-    if (book) {
-        let obj = book.reviews[username];
-
-        if(obj && obj.length > 0){
-            delete book.reviews[username]
-            return res.status(200).json({message: JSON.stringify(obj) +" , was deleted succesfully. "+ JSON.stringify(book)});
-        }else{
-            return res.status(404).json({message: "The review was not found!"} + JSON.stringify(obj))
-        }
-    } else {
-        return res.status(404).json({message: "Book not found!"});
-    }
-})
+   regd_users.put("/auth/review/:isbn", (req, res) => {
+       let isbn = req.params.isbn;
+       let review = req.query.review;
+       let username = req.session.authorization.username;
+       let book = findBookById(isbn);
+   
+       if (book) {
+           let obj = book.reviews;
+           let tipo = typeof(obj);
+           if(obj[username]){
+               obj[username] = review;
+               return res.status(200).json({message: "Review same user saved ***"+ tipo +"**** " + JSON.stringify(book)});
+           }else{
+               obj[username] = review;
+               return res.status(200).json({message: "Different user Review saved " + JSON.stringify(book)});
+           }
+       } else {
+           return res.status(404).json({message: "Book not found!"});
+       }
+   });
+   
+   regd_users.delete("/auth/review/:isbn", (req, res)=>{
+       let isbn = req.params.isbn;
+       let username = req.session.authorization.username;
+       let book = findBookById(isbn);
+   
+       if (book) {
+           let obj = book.review[username];
+   
+           if(obj && obj.length > 0){
+               delete book.review[username]
+               return res.status(200).json({message: JSON.stringify(obj) +" , was deleted succesfully. "+ JSON.stringify(book)});
+           }else{
+               return res.status(404).json({message: "The review was not found!"} + JSON.stringify(obj))
+           }
+       } else {
+           return res.status(404).json({message: "Book not found!"});
+       }
+   })
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
